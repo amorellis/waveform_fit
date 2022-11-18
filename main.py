efba4e73d89f3c9f6140c4e8dedbd1978aac7d65
Antônio Amorelli <a.amorelli@outlook.com>
2022-11-18 10:42:12 -0300

    Version 1 complete

--- main.py
+++ main.py
@@ -1,28 +1,91 @@
 import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Importar arquivos
 
-
-# Converter DF para listas de cada sinal
-
-
-# Aplica FFT
-
-
-# Identifica os 50 harmonicos
-
-
-# Salva em uma planilha
-
-
 files = os.listdir("files")
 
-for file_name in files:
-    file = pd.read_csv("files/" + file_name, sep = ";", skiprows = 1)
+
+for file in files:
+
+    # Abre o arquivo e converte para dataframe
     
-    time = file[file.columns[0]].values.tolist()
-    tensao_1 = file[file.columns[1]].values.tolist()
-    tensao_2 = file[file.columns[2]].values.tolist()
-    tensao_3 = file[file.columns[3]].values.tolist()
+    data = pd.DataFrame()
+    
+    data = pd.read_csv("files/" + file, sep = ";", decimal = ",", skiprows = 1)
+    
+    
+    # Converte os sinais para listas
+    
+    t = data[data.columns[0]].values.tolist()
+    u = data[data.columns[1]].values.tolist()
+    v = data[data.columns[2]].values.tolist()
+    w = data[data.columns[3]].values.tolist()
+    
+    
+    # Plota os sinais
+    
+    plt.plot(t[0:1000], u[0:1000], t[0:1000], v[0:1000], t[0:1000], w[0:1000])
+    plt.show()
+    
+    
+    # Faz a FFT dos sinais
+    
+    samples = len(t)
+    
+    
+    freq = np.fft.rfftfreq(samples, t[1])
+    
+    n = len(freq)
+    U = np.abs(np.fft.rfft(u))
+    V = np.abs(np.fft.rfft(v))
+    W = np.abs(np.fft.rfft(w))
+    
+    
+    
+    # Plota o espectro de frequencia dos sinais
+    
+    plt.plot(freq, U)
+    plt.show()
+    
+    
+    # Identificação dos harmonicos em cada FFT
+    
+    harm_u = []
+    harm_v = []
+    harm_w = []
+    
+    for i in range(1, len(freq)):
+        if U[i -1] < 0.1 and U[i] > 0.1:
+            harm_u.append(U[i])
+                  
+    for i in range(1, len(freq)):
+        if V[i -1] < 0.1 and V[i] > 0.1:
+            harm_v.append(V[i])
+            
+    for i in range(1, len(freq)):
+        if W[i -1] < 0.1 and W[i] > 0.1:
+            harm_w.append(W[i])
+            
+            
+    harm_u = list(map(lambda x:((x**2/2)**0.5)/n, harm_u))
+    harm_v = list(map(lambda x:((x**2/2)**0.5)/n, harm_v))
+    harm_w = list(map(lambda x:((x**2/2)**0.5)/n, harm_w))
+            
+    # Salva em CSV o resultado
+    
+    resultado = pd.DataFrame()
+    
+    resultado['Index'] = range(1, 51)
+    resultado['U'] = harm_u[0:50]
+    resultado['V'] = harm_v[0:50]
+    resultado['W'] = harm_w[0:50]
+    
+    resultado.to_csv("results/" + file, sep = ";", decimal = ",", index = False)
+
+
+
+
 
